7c77ac771304cd1e41c0ab89c45604adab5e419f
nv23014-mohammed-taha <167678507+nv23014-mohammed-taha@users.noreply.github.com>
2025-11-08 15:05:44 +0300

    Update weather_model.py

--- weather_model.py
+++ weather_model.py
@@ -1,15 +1,15 @@
-import os, cv2, numpy as np
+import os
+import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.applications.vgg19 import preprocess_input
-
-# Load pre-trained model
-MODEL_PATH = "vgg19.h5"  # replace with your trained model path
-model = load_model(MODEL_PATH)
-classes = ["cloudy","foggy","rainy","shine","sunrise"]
 
 def predict_weather_from_image(img_path):
+    import cv2  # lazy import to avoid Streamlit Cloud crash
+    MODEL_PATH = "vgg19.h5"
+    model = load_model(MODEL_PATH)
+    classes = ["cloudy","foggy","rainy","shine","sunrise"]
+
     img = cv2.imread(img_path)
-    img = cv2.resize(img,(150,150))
+    img = cv2.resize(img, (150,150))
     img = np.expand_dims(img, axis=0)/255.0
     pred = model.predict(img)
     class_idx = np.argmax(pred)
